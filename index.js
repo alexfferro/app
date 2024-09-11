8c2f7e08298b8fca5d1f39e47db7ffa7f98f6d21
Alex Ferro <alex.ferreira.ferro@gmail.com>
2024-09-11 08:25:36 -0300

    fundamento 003

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+let metas = [
+  {
+    value: 'Caminhar todos os dias',
+    checked: false
+  },
+  {
+    value: 'Estudar programação',
+    checked: false
+  },
+  {
+    value: 'Beber no mínimo 2L de água',
+    checked: false
+  },
+  {
+    value: 'Ler 1 (um) Livro por mês',
+    checked: false
+  }
+]
